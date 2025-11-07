968682017822d88f8e218da1d1034bbcdfb05a52
William Mattson Kheel Gomes Araújo <william.mattson02@gmail.com>
2025-11-06 22:32:45 -0400

    organização no código

--- app-vendas.py
+++ app-vendas.py
@@ -7,13 +7,13 @@ if 0 <= subtotal < 9:
     desconto = 0.00
     vfinal = subtotal
 elif 10 <= subtotal < 99:
-    desconto=0.05
+    desconto = 0.05
     vfinal = subtotal - subtotal * 0.05
 elif 100 <= subtotal < 999:
     desconto = 0.10
     vfinal = subtotal - subtotal * 0.10
-else: #valor for acima de 1000
+else:  # valor for acima de 1000
     vfinal = subtotal - subtotal * 0.15
 
 print('O valor sem desconto: R${:.2f}'.format(subtotal))
-print('O valor com desconto: R$ {:.2f} ({:.2f}% de desconto)'.format(vfinal,desconto))
+print('O valor com desconto: R$ {:.2f} ({:.2f}% de desconto)'.format(vfinal, desconto))
--- lanchonete.py
+++ lanchonete.py
@@ -13,29 +13,29 @@ print('[200]    Refrigerante Lata        [R$5,00]')
 print('[201]    Chá Gelado               [R$4,00]')
 
 while True:
-  cod= input('Entre com o Código desejado:')
-  if cod == "100":
-    acumulador = acumulador + 9
-  elif cod == "101":
-    acumulador = acumulador + 11
-  elif cod == "102":
-    acumulador = acumulador + 12
-  elif cod == "103":
-      acumulador = acumulador + 13
-  elif cod == "104":
-      acumulador = acumulador + 14
-  elif cod == "105":
-    acumulador = acumulador + 17
-  elif cod == "200":
-    acumulador = acumulador + 5
-  elif cod == "201":
-    acumulador = acumulador + 4
-  else:
-      print('Opção inválida')
-      continue
-  resposta= input('Deseja pedir mais alguma coisa? (S/N)')
-  if resposta == 'S':
-    continue
-  else:
-    print('O total a ser pago é: {:.2f}'.format(acumulador))
-    break
+    cod = input('Entre com o Código desejado:')
+    if cod == "100":
+        acumulador = acumulador + 9
+    elif cod == "101":
+        acumulador = acumulador + 11
+    elif cod == "102":
+        acumulador = acumulador + 12
+    elif cod == "103":
+        acumulador = acumulador + 13
+    elif cod == "104":
+        acumulador = acumulador + 14
+    elif cod == "105":
+        acumulador = acumulador + 17
+    elif cod == "200":
+        acumulador = acumulador + 5
+    elif cod == "201":
+        acumulador = acumulador + 4
+    else:
+        print('Opção inválida')
+        continue
+    resposta = input('Deseja pedir mais alguma coisa? (S/N)')
+    if resposta == 'S':
+        continue
+    else:
+        print('O total a ser pago é: {:.2f}'.format(acumulador))
+        break
